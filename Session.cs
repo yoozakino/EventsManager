49ada91dad435f00ddcea252d51dd8b5682328c9
yoozakino <162565028+yoozakino@users.noreply.github.com>
2026-01-30 10:27:42 +0300

    /

--- Session.cs
+++ Session.cs
@@ -4,10 +4,8 @@
     {
         public static Пользователи CurrentUser { get; set; }
 
-        // Авторизация = есть текущий пользователь
         public static bool IsAuth => CurrentUser != null;
 
-        // Имя роли (если пользователь загружен с Include(u => u.Роли))
         public static string RoleName => CurrentUser?.Роли?.Name ?? "";
     }
 }
